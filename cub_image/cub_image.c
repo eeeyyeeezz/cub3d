4995fa4a69438e65d47335466da5a5aeff3b6dde
Georgianne Morra <gmorra@er-c5.kzn.21-school.ru>
2021-02-05 23:10:32 +0300

    upd + to make float positions

--- cub_image/cub_image.c
+++ cub_image/cub_image.c
@@ -6,11 +6,12 @@
 /*   By: gmorra <gmorra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 17:13:47 by gmorra            #+#    #+#             */
-/*   Updated: 2021/01/31 22:22:19 by gmorra           ###   ########.fr       */
+/*   Updated: 2021/02/05 16:35:00 by gmorra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
+#include <unistd.h>
 
 typedef		struct data
 {
@@ -60,8 +61,9 @@ int			main(void)
 	mlx_win = mlx_new_window(mlx, 500, 500, "image3D");
 	data.img = mlx_new_image(mlx, 500, 500);
 	data.addr = mlx_get_data_addr(data.img, &data.bpp, &data.length, &data.end);
-	mlx_put_image_to_window(mlx, mlx_win, data.img, 50, 50);
-	fill_image(&data, 20, 20);
+	fill_image(&data, 500, 500);
+	mlx_put_image_to_window(mlx, mlx_win, data.img, 0, 0);
+	write(1, "open\n", 6);
 	mlx_loop(mlx);
 }
 
--- ft_mlx.c
+++ ft_mlx.c
@@ -6,7 +6,7 @@
 /*   By: gmorra <gmorra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 15:17:10 by gmorra            #+#    #+#             */
-/*   Updated: 2021/01/31 21:42:18 by gmorra           ###   ########.fr       */
+/*   Updated: 2021/02/05 21:45:40 by gmorra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static	void		draw_all_pixels(int x, int y, t_struct *global)
 	j = y * SCALE;
 	save = j;
 
-	my_mlx_pixel_put(global->data, global->map->position_x * SCALE, global->map->position_y * SCALE, 0xFF0000);
+	// my_mlx_pixel_put(global->data, global->map->position_x * SCALE, global->map->position_y * SCALE, 0xFF0000);
 	while (j < y * SCALE + SCALE)
 	{
 		i = x * SCALE;
@@ -42,7 +42,7 @@ static	void		draw_all_pixels(int x, int y, t_struct *global)
 		}
 		j++;
 	}
-	mlx_put_image_to_window(global->mlx, global->mlx_win, global->data->img, 600, 300);
+	// mlx_put_image_to_window(global->mlx, global->mlx_win, global->data->img, 0, 0);
 }
 
 static	void		draw(t_struct *global)
@@ -59,10 +59,28 @@ static	void		draw(t_struct *global)
 		{
 			if (global->cub_map[y][x] == '1')
 				draw_all_pixels(x, y, global);
+			if (global->cub_map[y][x] == 'N')
+				draw_all_pixels(global->map->position_x, global->map->position_y, global);
 			x++;
 		}
 		y++;
 	}
+}
+
+static void		ft_raycast(t_struct *global)
+{
+	int x;
+	int y;
+
+	x = global->map->position_x;
+	y = global->map->position_y;
+
+	while (global->cub_map[y][x] != '1')
+	{
+		my_mlx_pixel_put(global->data, x * SCALE, y * SCALE, 0xFF0000);
+		y--;
+	}
+	// mlx_put_image_to_window(global->mlx, global->mlx_win, global->data->img, 0, 0);
 }
 
 int			key_hook(int keycode, t_struct *global)
@@ -78,34 +96,14 @@ int			key_hook(int keycode, t_struct *global)
 		global->map->position_x += 1;
 	if (keycode == 123)
 		global->map->position_x -= 1;
-	mlx_clear_window(global->mlx, global->mlx_win);
+	// mlx_clear_window(global->mlx, global->mlx_win);
+	mlx_destroy_image(global->mlx, global->data->img);		// destroy_image segfault
+	global->data->img = mlx_new_image(global->mlx, 1600, 900);
+	global->data->addr = mlx_get_data_addr(global->data->img, &global->data->bpp, &global->data->length, &global->data->end);
+	ft_raycast(global);
 	draw(global);
+	mlx_put_image_to_window(global->mlx, global->mlx_win, global->data->img, 0, 0);
 	return (0);
-}
-
-int			ft_close(t_struct *global)
-{
-	mlx_destroy_window(global->mlx, global->mlx_win);
-	return (1);
-}
-
-void 		fill_image(t_struct *global, int width, int height)
-{
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (i < height)
-	{
-		while (j < width)
-		{
-			my_mlx_pixel_put(global->data, j, i, 0xFF0000);
-			j++;
-		}
-		i++;
-		j = 0;
-	}
 }
 
 void		ft_mlx(t_struct *global)
@@ -117,15 +115,13 @@ void		ft_mlx(t_struct *global)
 	x = 0;
 	if (!(global->mlx = mlx_init()))
 		return ;
-	global->mlx_win = mlx_new_window(global->mlx, 2000, 900, "cub3D");
-	// global->cube.img = mlx_new_image(global->mlx, 20, 20);
-	// fill_image(global,);
-	global->data->img = mlx_new_image(global->mlx, 2000, 900);
+	global->mlx_win = mlx_new_window(global->mlx, 1600, 900, "cub3D");
+	global->data->img = mlx_new_image(global->mlx, 1600, 900);
 	global->data->addr = mlx_get_data_addr(global->data->img, &global->data->bpp, &global->data->length, &global->data->end);
 	mlx_key_hook(global->mlx_win, key_hook, global);
-	// mlx_hook(global->mlx_win, 2, 1L<<0, ft_close, global);
 	draw(global);
-	mlx_put_image_to_window(global->mlx, global->mlx_win, global->data->img, 600, 300);
+	ft_raycast(global);
+	mlx_put_image_to_window(global->mlx, global->mlx_win, global->data->img, 0, 0);
 	write(1, "open\n", 5);
 	mlx_loop(global->mlx);
 }
