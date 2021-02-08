0d4518b7637654af82b553edd235f6f30ddc5217
Georgianne Morra <gmorra@et-e1.kzn.21-school.ru>
2021-02-08 20:12:05 +0300

    upd + lodev algorithm from cpp

---
+++ raycaster_flat.c
@@ -0,0 +1,217 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <mlx.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include <math.h>
+# include <unistd.h>
+
+
+#define screenWidth 1920
+#define screenHeight 1080
+#define mapWidth 24
+#define mapHeight 24
+
+typedef		struct s_data
+{
+	void		*img;
+	char		*addr;
+	int			bpp;
+	int			length;
+	int			end;
+}
+					t_data;
+
+typedef		struct s_whole
+{
+	t_data		data;
+	void 		*mlx;
+	void 		*mlx_win;
+}					t_struct;
+
+int worldMap[mapWidth][mapHeight]=
+{
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+static	void		my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->length + x * (data->bpp / 8));
+	*(unsigned int*)dst = color;
+}
+
+int main(int argc, char **argv)
+{
+	t_struct global;
+
+	double posX = 22, posY = 12;  //x and y start position
+	double dirX = -1, dirY = 0; //initial direction vector
+	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+
+	double time = 0; //time of current frame
+	double oldTime = 0; //time of previous frame
+
+
+	global.mlx = mlx_init();
+	global.mlx_win = mlx_new_window(global.mlx, screenWidth, screenHeight, "cub3D");
+	global.data.img = mlx_new_image(global.mlx, screenWidth, screenHeight);
+	global.data.addr = mlx_get_data_addr(global.data.img, &global.data.bpp, &global.data.length, &global.data.end);
+
+//   while(!done())
+//   {
+    for(int x = 0; x < screenWidth; x++)
+    {
+      //calculate ray position and direction
+      double cameraX = 2 * x / (double)screenWidth - 1; //x-coordinate in camera space
+      double rayDirX = dirX + planeX * cameraX;
+      double rayDirY = dirY + planeY * cameraX;
+      //which box of the map we're in
+      int mapX = (int)posX;
+      int mapY = (int)posY;
+
+      //length of ray from current position to next x or y-side
+      double sideDistX;
+      double sideDistY;
+
+       //length of ray from one x or y-side to next x or y-side
+      double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+	  double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+
+      double perpWallDist;
+
+      //what direction to step in x or y-direction (either +1 or -1)
+      int stepX;
+      int stepY;
+
+      int hit = 0; //was there a wall hit?
+      int side; //was a NS or a EW wall hit?
+      //calculate step and initial sideDist
+      if(rayDirX < 0)
+      {
+        stepX = -1;
+        sideDistX = (posX - mapX) * deltaDistX;
+      }
+      else
+      {
+        stepX = 1;
+        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+      }
+      if(rayDirY < 0)
+      {
+        stepY = -1;
+        sideDistY = (posY - mapY) * deltaDistY;
+      }
+      else
+      {
+        stepY = 1;
+        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+      }
+      //perform DDA
+      while (hit == 0)
+      {
+        //jump to next map square, OR in x-direction, OR in y-direction
+        if(sideDistX < sideDistY)
+        {
+          sideDistX += deltaDistX;
+          mapX += stepX;
+          side = 0;
+        }
+        else
+        {
+          sideDistY += deltaDistY;
+          mapY += stepY;
+          side = 1;
+        }
+        //Check if ray has hit a wall
+        if(worldMap[mapX][mapY] > 0) hit = 1;
+      }
+      //Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
+      if(side == 0) perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+      else          perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+
+      //Calculate height of line to draw on screen
+      int lineHeight = (int)(screenHeight / perpWallDist);
+
+      //calculate lowest and highest pixel to fill in current stripe
+      int drawStart = -lineHeight / 2 + screenHeight / 2;
+      if(drawStart < 0)drawStart = 0;
+      int drawEnd = lineHeight / 2 + screenHeight / 2;
+		if(drawEnd >= screenHeight)drawEnd = screenHeight - 1;
+		int y = 0;
+		while (y < screenHeight)
+		{
+			if (y >= drawStart && y <= drawEnd)
+				my_mlx_pixel_put(&global.data, x, y, 0xFF00FF);
+			y++;
+		}
+
+    }
+
+    //speed modifiers
+    double moveSpeed = 5.0; //the constant value is in squares/second
+    double rotSpeed = 3.0; //the constant value is in radians/second
+    //move forward if no wall in front of you
+    // if(keyDown(SDLK_UP))
+    // {
+    //   if(worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
+    //   if(worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
+    // }
+    // //move backwards if no wall behind you
+    // if(keyDown(SDLK_DOWN))
+    // {
+    //   if(worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
+    //   if(worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
+    // }
+    // //rotate to the right
+    // if(keyDown(SDLK_RIGHT))
+    // {
+    //   //both camera direction and camera plane must be rotated
+    //   double oldDirX = dirX;
+    //   dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
+    //   dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
+    //   double oldPlaneX = planeX;
+    //   planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
+    //   planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
+    // }
+    // //rotate to the left
+    // if(keyDown(SDLK_LEFT))
+    // {
+    //   //both camera direction and camera plane must be rotated
+    //   double oldDirX = dirX;
+    //   dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+    //   dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+    //   double oldPlaneX = planeX;
+    //   planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+    //   planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+    // }
+//   }
+	mlx_put_image_to_window(global.mlx, global.mlx_win, global.data.img, 0, 0);
+	write(1, "open\n", 5);
+	mlx_loop(global.mlx);
+}
